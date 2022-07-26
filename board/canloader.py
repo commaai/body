86784225d6573eae62e1aa2178851621ecb7fcdf
Igor Biletksyy <bs@privacy.im>
2022-07-25 20:27:53 -0700

    add knee to canloader

--- board/canloader.py
+++ board/canloader.py
@@ -5,6 +5,7 @@ import _thread
 from panda import Panda
 from panda.tests.pedal.canhandle import CanHandle
 
+
 def heartbeat_thread(p):
   while True:
     try:
@@ -12,6 +13,27 @@ def heartbeat_thread(p):
       time.sleep(0.5)
     except Exception:
       continue
+
+def flasher(p, addr, file):
+  p.can_send(addr, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x0a", 0)
+  time.sleep(0.1)
+  print("flashing", file)
+  code = open(file, "rb").read()
+  retries = 3 # How many times to retry on timeout error
+  while(retries+1>0):
+    try:
+      Panda.flash_static(CanHandle(p, 0), code)
+    except TimeoutError:
+      print("Timeout, trying again...")
+      retries -= 1
+    else:
+      print("Successfully flashed")
+      break
+
+def flush_panda():
+  while(1):
+    if len(p.can_recv()) == 0:
+      break
 
 
 if __name__ == "__main__":
@@ -23,16 +45,32 @@ if __name__ == "__main__":
   _thread.start_new_thread(heartbeat_thread, (p,))
   p.set_safety_mode(Panda.SAFETY_BODY)
 
-  while 1:
-    if len(p.can_recv()) == 0:
-      break
+  is_base_found = False
+  is_knee_found = False
 
-  p.can_send(0x250, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x0a", 0)
+  print("Looking for boards to flash...")
+  start_time = time.time()
+  received_msgs = []
+  while (time.time() - start_time) < 1:
+    buff = p.can_recv()
+    if len(buff):
+      received_msgs.extend(buff)
 
-  if args.fn:
-    time.sleep(0.1)
-    print("flashing", args.fn)
-    code = open(args.fn, "rb").read()
-    Panda.flash_static(CanHandle(p, 0), code)
+  for msg in received_msgs:
+    if msg[0] == 0x203:
+      is_base_found = True
+    elif msg[0] == 0x303:
+      is_knee_found = True
 
-  print("can flash done")
+  if is_knee_found and args.fn:
+    print("Flashing knee motherboard")
+    flasher(p, 0x350, args.fn)
+
+  time.sleep(0.2)
+  flush_panda()
+
+  if is_base_found and args.fn:
+    print("Flashing base motherboard")
+    flasher(p, 0x250, args.fn)
+
+  print("CAN flashing done")
