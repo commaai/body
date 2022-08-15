1fc01d70715aa943dc6c95366f993a706a5d2a2e
Igor Biletksyy <bs@privacy.im>
2022-08-15 14:43:39 -0700

    can flasher

--- board/canloader.py
+++ board/canloader.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import time
 import argparse
 import _thread
@@ -14,10 +15,16 @@ def heartbeat_thread(p):
     except Exception:
       continue
 
+def flush_panda():
+  while(1):
+    if len(p.can_recv()) == 0:
+      break
+
 def flasher(p, addr, file):
   p.can_send(addr, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x0a", 0)
   time.sleep(0.1)
   print("flashing", file)
+  flush_panda()
   code = open(file, "rb").read()
   retries = 3 # How many times to retry on timeout error
   while(retries+1>0):
@@ -30,47 +37,23 @@ def flasher(p, addr, file):
       print("Successfully flashed")
       break
 
-def flush_panda():
-  while(1):
-    if len(p.can_recv()) == 0:
-      break
-
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Flash body over can')
+  parser.add_argument("board", type=str, nargs='?', help="choose base or knee")
   parser.add_argument("fn", type=str, nargs='?', help="flash file")
   args = parser.parse_args()
+
+  assert args.board in ["base", "knee"]
+  assert os.path.isfile(args.fn)
+
+  addr = 0x250 if args.board == "base" else 0x350
 
   p = Panda()
   _thread.start_new_thread(heartbeat_thread, (p,))
   p.set_safety_mode(Panda.SAFETY_BODY)
 
-  is_base_found = False
-  is_knee_found = False
-
-  print("Looking for boards to flash...")
-  start_time = time.time()
-  received_msgs = []
-  while (time.time() - start_time) < 1:
-    buff = p.can_recv()
-    if len(buff):
-      received_msgs.extend(buff)
-
-  for msg in received_msgs:
-    if msg[0] == 0x203:
-      is_base_found = True
-    elif msg[0] == 0x303:
-      is_knee_found = True
-
-  if is_knee_found and args.fn:
-    print("Flashing knee motherboard")
-    flasher(p, 0x350, args.fn)
-
-  time.sleep(0.2)
-  flush_panda()
-
-  if is_base_found and args.fn:
-    print("Flashing base motherboard")
-    flasher(p, 0x250, args.fn)
+  print("Flashing motherboard")
+  flasher(p, addr, args.fn)
 
   print("CAN flashing done")
--- board/flash_can.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-scons -u
-
-./canloader.py obj/body.bin.signed
