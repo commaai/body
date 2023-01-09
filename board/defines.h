19ace8d0a17403c100ec7efbb17c19d6cc45c20f
Willem Melching <willem.melching@gmail.com>
2023-01-09 17:05:08 +0100

    fix multiple definition of `fault_status`

--- board/defines.h
+++ board/defines.h
@@ -178,11 +178,11 @@ typedef struct {
 
 } board_t;
 
-struct {
+typedef struct {
     uint8_t left_i2c : 1;
     uint8_t left_angle : 1;
     uint8_t right_i2c : 1;
     uint8_t right_angle : 1;
-} fault_status;
+} fault_status_t;
 
 #endif // DEFINES_H
